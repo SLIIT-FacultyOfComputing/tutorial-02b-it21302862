e7a1e93b3483464d442366d7123e0725d7bfb966
Sri Samadhi L.A.S.S <100861562+it21302862@users.noreply.github.com>
2022-03-03 17:46:51 +0000

    tutorial 2

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,17 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no)
+  {
+   long fac=1;
+    for(int i=no;i>=1;i--){
+      fac=fac*i;
+      
+    }
+    return fac;
+  }
+  long nCr(int n, int r)
+  {
+  return Factorial(n)/(Factorial(r)*Factorial(n-r));
+  }
